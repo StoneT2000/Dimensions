f333922184588dbf1820756ea1a8065ef6554b3b
StoneT2000 <stoneztao@gmail.com>
2020-03-29 19:45:35 -0700

    Naming

--- src/Station/index.ts
+++ src/Station/index.ts
@@ -3,13 +3,19 @@ import path from 'path';
 
 export class Station {
   public app: express.Application;
-  constructor() {
+  public static _id: number = 0;
+  public id: number = 0;
+  public name: string;
+  constructor(name?: string) {
     this.app = express();
     this.app.use(express.static(path.join(__dirname, 'web/build')));
     this.app.get('/', function(req, res) {
       res.sendFile(path.join(__dirname, 'web/build', 'index.html'));
     });
     this.app.listen(9000);
-    console.log("Running station at localhost:9000");
+    this.id = Station._id;
+    this.name = `Station_${this.id}`;
+    Station._id++;
+    console.log(`Running ${this.name} at localhost:9000`);
   }
 }
