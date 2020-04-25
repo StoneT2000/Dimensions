b3bda3d16bff4aad0ce038f748e24f388e49196b
StoneT2000 <stoneztao@gmail.com>
2020-04-25 00:18:02 -0700

    Initial documentation and comments for design override options

--- src/Design/index.ts
+++ src/Design/index.ts
@@ -47,6 +47,13 @@ export abstract class Design {
          * }
          */
       }
+    },
+    override: {
+      active: false,
+      command: 'echo NO COMMAND PROVIDED',
+      conclude_command: 'D_MATCH_FINISHED',
+      arguments: []
+
     }
   }
 
@@ -132,6 +139,79 @@ export abstract class Design {
    */
   abstract async getResults(match: Match): Promise<any>
 
+  public createCustom({
+
+  }) {
+
+  }
+
+}
+
+export module Design {
+  
+  /**
+   * The override options interface
+   * This is used to provide configurations for a custom {@link Design} outside the scope and infrastructure of 
+   * Dimensions
+   */
+  export interface OverrideOptions {
+    /**
+     * Whether or not to use the override configurations
+     * @default `false`
+     */
+    active: boolean,
+
+    /**
+     * The command to run that will run a match and send to the standard out any updates and
+     * and match conclusion data. This is always executed in the same directory the dimension is created in.
+     * @example ./run.sh
+     * @example python3 path/to/matchrunner/run.py
+     * @default `echo NO COMMAND PROVIDED`
+     */
+    command: string,
+
+    /**
+     * An array of arguments to pass in to the script command
+     * NOTE, there are a few special strings when put into this array, will be populated with dynamic data. 
+     * See {@link DynamicDataStrings} for what is available and what they do
+     * @example ['--debug=false', 'D_FILES', '--replay-directory=my_replays', '--some-option=400']
+     */
+    arguments: Array<string | Design.DynamicDataStrings>
+
+    /**
+     * The command telling the engine that the match is done and should now process the next lines as results
+     * @default `D_MATCH_FINISHED`
+     */
+    conclude_command: string,
+  }
+
+  /**
+   * Dynammic Data strings are strings in the {@link OverrideOptions} arguments array that are automatically replaced
+   * with dynamic data as defined in the documentation of these enums
+   */
+  export enum DynamicDataStrings {
+    /**
+     * D_FILES is automatically populated by a space seperated string list of the file paths provided for each of the 
+     * bots competing in a match. 
+     * NOTE, these paths don't actually need to be files, they can be directories or anything that works with 
+     * your own command and design
+     */
+    D_FILES = 'D_FILES',
+
+    /**
+     * D_AGENT_IDS is automatically populated by a space seperated string list of the agent IDs of every agent being
+     * loaded into a match in the same order as D_FILES. This should always be sorted by default as agents are loaded
+     * in order from agent ID `0` to agent ID `n`
+     */
+    D_AGENT_IDS = 'D_AGENT_IDS',
+
+    /**
+     * D_TOURNAMENT_IDS is automatically populated by a space seperated string list of the tournament ID numbers of
+     * the agents being loaded into the match in the same order. If no tournament is being run all the ID numbers will 
+     * default to 0 but still be passed in to the command you give for the override configurations
+     */
+    D_TOURNAMENT_IDS = 'D_TOURNAMENT_IDS'
+  }
 }
 
 /**
@@ -139,5 +219,11 @@ export abstract class Design {
  */
 export interface DesignOptions {
   /** The default engine options to use for all matches created using this design */
-  engineOptions: EngineOptions
+  engineOptions: EngineOptions,
+
+  /** 
+   * Override configurations if user wants to run matches with a non-dimensions based design and run their own design
+   * in another language
+   */
+  override: Design.OverrideOptions
 };
