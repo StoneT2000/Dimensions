e0dc2b15c72aae2170fb9b47ac15d07d7e11349d
StoneT2000 <stonezt2019@gmail.com>
2020-07-04 19:16:30 -0700

    added simple database running test

---
+++ tests/core/database/01-database.tourney.spec.ts
@@ -0,0 +1,58 @@
+import * as Dimension from '../../../src';
+import { RockPaperScissorsDesign } from '../../rps';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import chaiSubset from 'chai-subset';
+import sinonChai from "sinon-chai";
+import sinon from "sinon";
+import 'mocha';
+import { Logger, MatchEngine, Match, Agent, Design, Tournament, DError, MongoDB } from '../../../src';
+import { sleep } from '../utils/sleep';
+import { createLadderTourney } from '../tourney/utils';
+const expect = chai.expect;
+chai.should()
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+chai.use(chaiSubset);
+
+describe('Testing Database with Tournament', () => {
+  const paper = {file: './tests/kits/js/normal/paper.js', name: 'paper'};
+  const rock = {file: './tests/kits/js/normal/rock.js', name: 'rock'};
+  const disabled = {file: './tests/kits/js/normal/rock.js', name: 'disabled', existingID:'disabled'};
+  const testbot = {file: './tests/kits/js/normal/paper.js', name: 'test', existingID: 'test'};
+  const botList = [rock, paper];
+  const rpsDesign = new RockPaperScissorsDesign('RPS');
+  const d = Dimension.create(rpsDesign, {
+    activateStation: false,
+    observe: false,
+    id: "12345678",
+    loggingLevel: Logger.LEVEL.NONE,
+    defaultMatchConfigs: {
+      bestOf: 21
+    }
+  });
+  let mongo = new MongoDB('mongodb://root:rootpassword@mongodb_container:27017/test?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false');
+  
+  before( async () => {
+    await d.use(mongo);
+  });
+  describe("Test running", () => {
+    it("should run", async () => {
+      let tourney = createLadderTourney(d, botList, {
+        name: "Ladder Tournament",
+        id: "12345"
+      });
+      await tourney.run();
+      await sleep(2000);
+      let ranks = await tourney.getRankings();
+      expect(tourney.state.statistics.totalMatches).to.be.greaterThan(1);
+      expect(ranks[0].player.file).to.equal(paper.file);
+      expect(ranks[1].player.file).to.equal(rock.file);
+    });
+  });
+    
+  after(() => {
+    d.cleanup();
+    mongo.db.close();
+  });
+});
