3611f7c1c3681e34284a9befbcd16772f1207a25
Stone Tao <stonezt2019@gmail.com>
2020-08-20 02:16:55 -0700

    Update 03-engine.memorylimit.spec.ts

--- tests/core/engine/03-engine.memorylimit.spec.ts
+++ tests/core/engine/03-engine.memorylimit.spec.ts
@@ -48,34 +48,34 @@ describe('Testing MatchEngine Memory Limit Mechanism', () => {
     }
 
     // TODO, investigate why circleci can't get this passing
-    // it.skip("should allow for custom memory limit reached functions", async () => {
-    //   const someMessage = "some message";
-    //   let customRan = false;
-    //   const customEngineOptions: DeepPartial<MatchEngine.EngineOptions> = {
-    //     memory: {
-    //       limit: 1024 * 1024 * 4,
-    //       memoryCallback: (agent: Agent, match: Match, engineOptions: MatchEngine.EngineOptions) => {
-    //         match.kill(agent.id);
-    //         match.log.detail(`custom message! - agent ${agent.id} - '${agent.name}' reached max memory!`);
-    //         customRan = true;
-    //         // engine options provided should be the same as the match itself
-    //         expect(match.matchEngine.getEngineOptions()).to.be.equal(engineOptions);
-    //       }
-    //     }
-    //   }
-    //   let sandbox = sinon.createSandbox();
+    it.skip("should allow for custom memory limit reached functions", async () => {
+      const someMessage = "some message";
+      let customRan = false;
+      const customEngineOptions: DeepPartial<MatchEngine.EngineOptions> = {
+        memory: {
+          limit: 1024 * 1024 * 4,
+          memoryCallback: (agent: Agent, match: Match, engineOptions: MatchEngine.EngineOptions) => {
+            match.kill(agent.id);
+            match.log.detail(`custom message! - agent ${agent.id} - '${agent.name}' reached max memory!`);
+            customRan = true;
+            // engine options provided should be the same as the match itself
+            expect(match.matchEngine.getEngineOptions()).to.be.equal(engineOptions);
+          }
+        }
+      }
+      let sandbox = sinon.createSandbox();
       
-    //   let match = await d.createMatch(['./tests/kits/js/normal/rock.exceedmemory.js', './tests/kits/js/normal/paper.js'], {
-    //     bestOf: 1001,
-    //     engineOptions: customEngineOptions
-    //   });
-    //   let killSpy = sandbox.spy(match, 'kill');
-    //   let logspy = sandbox.spy(match.log, 'detail');
-    //   let results = await match.run();
-    //   expect(results.terminated[0]).to.equal('terminated');
-    //   expect(killSpy).to.be.calledWith(0)
-    //   expect(customRan).to.equal(true, "custom memory callback ran");
-    // });
+      let match = await d.createMatch(['./tests/kits/js/normal/rock.exceedmemory.js', './tests/kits/js/normal/paper.js'], {
+        bestOf: 1001,
+        engineOptions: customEngineOptions
+      });
+      let killSpy = sandbox.spy(match, 'kill');
+      let logspy = sandbox.spy(match.log, 'detail');
+      let results = await match.run();
+      expect(results.terminated[0]).to.equal('terminated');
+      expect(killSpy).to.be.calledWith(0)
+      expect(customRan).to.equal(true, "custom memory callback ran");
+    });
 
     it("should allow for turning off memory limit", async () => {
       let match = await d.createMatch(['./tests/kits/js/normal/rock.js', './tests/kits/js/normal/paper.js'], {
