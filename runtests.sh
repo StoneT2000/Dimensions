cf6799bc2ce88acaef5d7e975ffe26493f94bb6a
Stone Tao <stonezt2019@gmail.com>
2020-08-20 22:14:00 -0700

    try new test format

--- runtests.sh
+++ runtests.sh
@@ -8,5 +8,12 @@ npm run clean
 npm run test-seed
 
 # test core, then api
-nyc --reporter=html --no-clean mocha tests/core/**/*.spec.ts
+nyc --reporter=html --no-clean mocha tests/core/agent/**/*.spec.ts
+nyc --reporter=html --no-clean mocha tests/core/database/**/*.spec.ts
+nyc --reporter=html --no-clean mocha tests/core/dimension/**/*.spec.ts
+nyc --reporter=html --no-clean mocha tests/core/engine/**/*.spec.ts
+nyc --reporter=html --no-clean mocha tests/core/logger/**/*.spec.ts
+nyc --reporter=html --no-clean mocha tests/core/rating/**/*.spec.ts
+nyc --reporter=html --no-clean mocha tests/core/tourney/**/*.spec.ts
+nyc --reporter=html --no-clean mocha tests/api/**/*.spec.ts
 
