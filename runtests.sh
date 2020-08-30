7d63c7ed7d5ad045c1be721b5ca201f73a9e3bc6
Stone Tao <stonezt2019@gmail.com>
2020-08-30 16:17:29 -0700

    separate tests

--- runtests.sh
+++ runtests.sh
@@ -15,7 +15,7 @@ tests/core/engine/04*.spec.ts \
 tests/core/utilstest/**/*.spec.ts \
 tests/core/agent/**/*.spec.ts \
 tests/core/database/**/*.spec.ts \
-tests/core/storage/**/*.spec.ts \
+# tests/core/storage/**/*.spec.ts \
 tests/core/dimension/**/*.spec.ts \
 tests/core/logger/**/*.spec.ts \
 tests/core/match/**/*.spec.ts \
