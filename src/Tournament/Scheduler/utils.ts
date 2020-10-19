82f9a59d43f27e00b72440950a42be532abb4b13
Stone Tao <stonezt2019@gmail.com>
2020-10-18 22:39:54 -0700

    fix scheduler not pitting adjacent ranekd bots against each other

--- src/Tournament/Scheduler/utils.ts
+++ src/Tournament/Scheduler/utils.ts
@@ -9,7 +9,7 @@ export const chooseKRandomElements = <T>(
     reservoir.push(arr[i]);
   }
   for (let i = k; i < arr.length; i++) {
-    const j = Math.floor(Math.random() * i);
+    const j = Math.round(Math.random() * i);
     if (j < k) {
       reservoir[j] = arr[i];
     }
