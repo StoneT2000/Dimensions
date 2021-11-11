5ea54c368303b8621ee4e27faf7d972c78b686f5
stone_tao <stonezt2019@gmail.com>
2021-11-10 16:35:21 -0800

    Update agent.spec.ts

--- tests/core/agent.spec.ts
+++ tests/core/agent.spec.ts
@@ -10,7 +10,7 @@ chai.should();
 chai.use(chaiAsPromised);
 chai.use(chaiSubset);
 
-describe('Testing Agents', () => {
+describe.only('Testing Agents', () => {
   /**
    * Test the environments created by makeEnv
    *
@@ -41,6 +41,37 @@ describe('Testing Agents', () => {
         -9.364599159415079,
         1e-15
       );
+    });
+    it('should run python vs js', async () => {
+      const env = await dim.makeEnv(rpsenv, {
+        max_cycles: 30,
+      });
+      const agentjs = dim.addAgent({
+        agent: path.join(__dirname, '../envs/rps/agents/paper.js'),
+        processOptions: {
+          time: {
+            perStep: 1000,
+            overage: 0,
+          },
+        },
+        location: 'docker',
+      });
+      const agentpy = dim.addAgent({
+        agent: path.join(__dirname, '../envs/rps/agents/agent.py'),
+        processOptions: {
+          time: {
+            perStep: 1000,
+            overage: 0,
+          },
+        },
+        location: 'docker',
+      });
+      const r1 = await dim.runEpisode(env, [agentjs, agentpy], 0);
+      for (const playerID in r1.results.final.data) {
+        expect(r1.results.final.data[playerID].done).to.equal(true);
+      }
+      // paper beats rock 30/30 times.
+      expect(r1.results.final.data['player_0'].info.score).to.equal(30);
     });
   });
   describe('Test docker agents', () => {
